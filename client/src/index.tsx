2655820ee0e772021d4c5ad16e07a5888901c882
benjdg16dev <benjdg16dev@gmail.com>
2023-04-13 04:09:34 -0600

    [feat] client redux setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import authReducer from "./state";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+// * NOTE: Following imports are used for persisting state on localStorage
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // * NOTE: Just to ignore a warning if you are using persist
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
---
+++ client/src/state/index.tsx
@@ -0,0 +1,60 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// TODO: Refactor any keyword
+interface IInitialState {
+  mode: string;
+  user: any | null;
+  token: string | null;
+  posts: Array<any>;
+}
+
+const initialState: IInitialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  /**
+   * Reducers are just functions that modifies state.
+   * ! You should not modify initialState directly
+   * However, toolkit is built on top of Immer which handles it under the hood
+   */
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("Error: User friends non-existent.");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post_id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+export default authSlice.reducer;
